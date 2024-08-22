be16088e004869f50047f13a76b17648f35f52a5
Truong Giang <truong.giang@thoughtworks.com>
2024-08-22 19:26:44 +0700

    Add export secret action

--- .github/actions/prep/entrypoint.sh
+++ .github/actions/prep/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# echo $VAULT_PASS > /vault_pass.txt
-# echo $(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1 >> $GITHUB_ENV 
-# rm /vault_pass.txt
+echo $VAULT_PASS > /vault_pass.txt
+echo $(ansible-vault view --vault-password-file=/vault_pass.txt env-secrets)|xargs -n1 >> $GITHUB_ENV 
+rm /vault_pass.txt
